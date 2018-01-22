15b993547c5486550180d4c7770628f6d0c0d5ad
ism3769892 <ism3769892@a18.informatica.escoladeltreball.org>
2018-01-22 13:51:26 +0100

    First blog

---
+++ hola.py
@@ -0,0 +1,25 @@
+#coding: utf­8
+
+print "Hola MUNDO"
+edad = 16
+print "Tu edad es: " , edad , " años"
+
+nombre = "Jalila"
+print "Tu nombre es: " , nombre , "Mustapha"
+
+edad = input("Indique su edad: ")
+if edad >= 18:
+    print "Eres mayor de edad. Puedes pasar"
+else:
+    print "Lo siento con" , edad , "no puedes pasar"  
+
+nombre = raw_input("Indique su nombre: ")
+if not nombre == "Jalila Mustapha" :
+    print "Nooo..."
+    print "...ooooo..."
+    print "...ooo"
+else:
+   print "Si" , "es", nombre 
+
+#
+
